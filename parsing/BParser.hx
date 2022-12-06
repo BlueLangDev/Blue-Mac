ac6880f3c4f5901842a3e08f1557f78e352e6e1c
Stephen <90519370+magnumsrt@users.noreply.github.com>
2022-12-06 18:13:26 -0500

    Fix parser

--- parsing/BParser.hx
+++ parsing/BParser.hx
@@ -7,8 +7,6 @@ import languageutils.coffeescript.BCoffeeScriptUtil;
 import languageutils.haxe.BHaxeUtil;
 import languageutils.groovy.BGroovyUtil;
 import lexing.BLexer.BToken;
-import customtargetutils.CustomTarget;
-
 typedef ASTStruct = {
 	var token:BToken;
 	var iterator:Dynamic;
@@ -212,10 +210,6 @@ class BParser {
 			case "javascript":
 				BJSUtil.toJs(serializedResult);
 				BJSUtil.buildJsFile();
-
-			default:
-				CustomTarget.toStrings(serializedResult);
-				CustomTarget.buildFile();
 		}
 
 		token = null;
