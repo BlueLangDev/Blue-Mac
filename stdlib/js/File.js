cfce62b30ea5c4ac7cbe0f81dc7cb3230620f2c1
Stephen <90519370+magnumsrt@users.noreply.github.com>
2022-12-21 10:28:49 -0500

    Fix file reading js

--- stdlib/js/File.js
+++ stdlib/js/File.js
@@ -1,25 +1,19 @@
-const fs = import('fs');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class File {
     static read(file) {
-        var filei = new XMLHttpRequest();
-        filei.open("GET", file, false);
-        filei.onreadystatechange = function () {
-            if (filei.readyState === 4) {
-                if (filei.status === 200 || filei.status == 0) {
-                    var recieved = filei.responseText;
-                    return recieved;
-                }
-            }
-        }
-        filei.send(null);
+        return fs.readFileSync(file, 'utf8');
     }
 
     static write(file, content) {
-        fs.writeFile(file, content, err => {
-            if (err) {
-                console.error(err);
-            }
-        })
+        fs.open(file, "w", (error, fd) => {
+            fs.writeFile(file, content, err => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            fs.close(fd);
+        });
     }
 }
